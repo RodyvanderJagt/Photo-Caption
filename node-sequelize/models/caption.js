bebb76037fcfaa6c48e9a160da55c0b8f3990da9
Rody van der Jagt <rodyvanderj8@gmail.com>
2021-08-02 11:41:33 +0200

    Sequelize models and migrations

---
+++ node-sequelize/models/caption.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Caption extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Caption.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        as: 'user'
+      });
+      Caption.belongsTo(Models.Image, {
+        foreignKey: 'image_id',
+        as: 'image'
+      })
+    }
+  };
+  Caption.init({
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    image_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    caption: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Caption',
+  });
+  return Caption;
+};
